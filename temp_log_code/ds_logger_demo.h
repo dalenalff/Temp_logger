65ecad23e190115e5bd24e0694b129f433f2e9c3
dalenalff <67212208+dalenalff@users.noreply.github.com>
2023-04-14 14:03:28 -0500

    Update ds_logger_demo.h

--- temp_log_code/ds_logger_demo.h
+++ temp_log_code/ds_logger_demo.h
@@ -1,5 +1,5 @@
-#define CONVERT_T 0x44
-#define READ_ROM 0x33
-#define IS_DS18B20_SENSOR 0x28
-#define SKIP_ROM 0xCC
-#define READ_SCRATCHPAD 0xBE
+#define CONVERT_T 0x44           // Temperature conversion
+#define READ_ROM 0x33            // Read the ROM code of DS18B20 sensor
+#define IS_DS18B20_SENSOR 0x28   // Code to confirm a sensor is DS18B20
+#define SKIP_ROM 0xCC            // Directly communicate with DS18B20 sensor
+#define READ_SCRATCHPAD 0xBE     // read the scratchpad memory of DS18B20 sensor
